e331e7c95f0395ff4c19598d296b72b0b5c3c217
James S <i@nixeagle.org>
2010-06-18 23:40:42 +0000

    Initial commit, does really nothing interesting

---
+++ juggler.lisp
@@ -0,0 +1,42 @@
+(defpackage #:nixeagle.juggler
+  (:use :cl :nutils))
+(in-package :nixeagle.juggler)
+
+(defstruct (3d-vector
+             (:constructor make-3d-vector (x y z)))
+  "Models a vector in 3d.
+
+This is a struct mostly for speed reasons, these act like C arrays but
+with much greater type safety built in."
+  (x nil :type real)
+  (y nil :type real)
+  (z nil :type real))
+
+(defun vector-add (vector1 vector2)
+  "Adds two vectors."
+  (declare (3d-vector vector1 vector2))
+  (with-slots (x1 y1 z1) vector1
+    (with-slots (z2 y2 z2) vector2
+      (make-3d-vector (+ x1 x2) (+ x1 x2) (+ x1 x2)))))
+
+(defun magnitude (vector)
+  (declare (3d-vector vector))
+  (with-slots (x y z) vector
+    (sqrt (+ (expt x 2) (expt y 2) (expt z 2)))))
+
+(defun scale (vector scale-factor)
+  (declare (3d-vector vector)
+           (real scale-factor))
+  (with-slots (x y z) vector
+    (make-3d-vector (* x scale-factor) (* y scale-factor) (* z scale-factor))))
+
+(defun negate (vector)
+  "Invert VECTOR by multiplying by -1."
+  (declare (3d-vector vector))
+  (scale vector -1))
+
+(defun unit-vector (vector)
+  "Compute unit vector of VECTOR."
+  (declare (3d-vector vector))
+  (scale vector (/ 1 (magnitude vector))))
+
