df7b57db39d72872da94a4b96c4dece9a3c5d353
James S <i@nixeagle.org>
2010-06-21 22:16:03 +0000

    Document WIDTH HEIGHT

--- src/globals.lisp
+++ src/globals.lisp
@@ -1,7 +1,10 @@
 (in-package :juggler)
 
-(defvar *width* 640)
-(defvar *height* 360)
+(defvar *width* 640
+  "Width of the image in pixels.")
+(defvar *height* 360
+  "Height of the image in pixels.")
+
 (defvar *gamma* 2.2)
 (defvar *second_millis* 1000)
 (defvar *image_scale* 3)
