61d1c5fb15e28675bef73251ec536d0e459a4d9d
James S <i@nixeagle.org>
2010-06-21 20:25:38 +0000

    Define NEXT-RANDOM-FLOAT

--- src/random.lisp
+++ src/random.lisp
@@ -8,3 +8,16 @@
   (apply #'vector
    (loop for i from 0 to (1- size)
       collect (random number))))
+
+(let ((vector (make-random-vector 10000))
+      (i 0))
+  (let ((vector-length (length vector)))
+    (declare ((integer 0 #.(1- most-positive-fixnum)) i vector-length))
+    (defun next-random-float ()
+      "Grab a float from vector of pregenerated floats."
+      (declare (optimize (speed 3) (safety 0) (debug 0)))
+      (multiple-value-prog1 (svref vector i)
+        (when (= vector-length (the fixnum (incf i)))
+          (setf i 0))))))
+
+;;; END
