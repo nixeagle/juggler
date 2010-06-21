d7d7d2ae0564ba04fee34efe1eb0e179dc256088
James S <i@nixeagle.org>
2010-06-21 22:22:52 +0000

    Define *EYE* and *CENTER*

--- src/globals.lisp
+++ src/globals.lisp
@@ -8,3 +8,13 @@
 (defvar *gamma* 2.2)
 (defvar *second_millis* 1000)
 (defvar *image_scale* 3)
+
+
+(defvar *eye* #(0 0 0)
+  "Location where we are looking from.
+
+Right now we will treat this as the origin vector.")
+
+(declaim ((vector (member 0)) *center*))
+(defvar *center* #(0 0 0)
+  "This is supposed to be the origin.")
